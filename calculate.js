d013b6f72b14bc281c7959e9ee691b7829df6290
wescley <wescleypbpe@gmail.com>
2022-04-27 10:03:48 -0300

    first commit from the points calculator

---
+++ calculate.js
@@ -0,0 +1,15 @@
+let select = document.getElementById("product");
+
+
+function fOcorrenci(){
+let flag = document.getElementById("product").value;
+ document.getElementById("vtext").innerHTML = flag + " "+ "Pontos.";
+ 
+}
+
+function fOcorrenci2(){
+let flag2 = document.getElementById("product2").value;
+let vInput = document.getElementById("value").value;
+ document.getElementById("vtext2").innerHTML = flag2 * vInput + " "+ "Pontos.";
+ 
+}
