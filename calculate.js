3767c9642a658c2f08e869611e2ed0edc81411ae
wescley <wescleypbpe@gmail.com>
2022-04-28 12:56:17 -0300

    changes to CSS and HTML.

--- calculate.js
+++ calculate.js
@@ -1,15 +1,17 @@
 let select = document.getElementById("product");
 
 
-function situations (){
-let vSituations = document.getElementById("product").value;
- document.getElementById("vtext").innerHTML = vSituations + " "+ "Pontos.";
- 
+
+function situations() {
+    let vSituations = document.getElementById("product").value;
+    document.getElementById("vtext").innerHTML = vSituations + " " + "Pontos.";
+
 }
 
-function arrests(){
-let vArrests = document.getElementById("product2").value;
-let vInput = document.getElementById("value").value;
- document.getElementById("vtext2").innerHTML = vArrests * vInput + " "+ "Pontos.";
- 
+function arrests() {
+    let vArrests = document.getElementById("product2").value;
+    let vInput = document.getElementById("value").value;
+    document.getElementById("vtext2").innerHTML = vArrests * vInput + " " + "Pontos.";
+
 }
+
