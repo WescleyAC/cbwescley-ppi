42c247ab1c42169bf6dacf2abdc115e135627e37
wescley <wescleypbpe@gmail.com>
2022-05-06 16:08:49 -0300

    changes to html, css and javascript

--- calculate.js
+++ calculate.js
@@ -6,6 +6,8 @@ function situations() {
     let vSituations = document.getElementById("product").value;
     document.getElementById("vtext").innerHTML = vSituations + " " + "Pontos.";
 
+   
+
 }
 
 function arrests() {
@@ -13,5 +15,18 @@ function arrests() {
     let vInput = document.getElementById("value").value;
     document.getElementById("vtext2").innerHTML = vArrests * vInput + " " + "Pontos.";
 
-}
+    if (vArrests === "5" && vInput >= 1  || vArrests === "10" && vInput >= 1 || vArrests === "12" && vInput >= 1 ){
 
+        document.getElementById("vtext3").innerHTML = "Mais 10 pontos pelo ponto debelado"
+    } 
+
+    else if (vArrests === "5" && vInput <= 0 || vArrests === "10" && vInput <= 0 || vArrests === "12" && vInput <= 0 ){
+
+        document.getElementById("vtext2").innerHTML = "--> Digite a quantidade em gramas " 
+      
+     }
+    
+    
+ 
+     
+}
