8b4e03d5c79fc4053ad9f4c170b8e7022948d610
wescley <wescleypbpe@gmail.com>
2022-05-17 14:13:20 -0300

    change where I add the total amount

--- calculate.js
+++ calculate.js
@@ -1,19 +1,22 @@
-let select = document.getElementById("product");
 
+function situations( ) {
+   let vSituations = document.getElementById("product").value;
+   document.getElementById("vtext").innerHTML = vSituations + " Pontos."; 
 
-
-function situations() {
-    let vSituations = document.getElementById("product").value;
-    document.getElementById("vtext").innerHTML = vSituations + " " + "Pontos.";
-
-   
+return vSituations
 
 }
+
+
 
 function arrests() {
     let vArrests = document.getElementById("product2").value;
     let vInput = document.getElementById("value").value;
-    document.getElementById("vtext2").innerHTML = vArrests * vInput + " " + "Pontos.";
+    parseInt(value2 = vArrests * vInput);
+    
+
+
+    document.getElementById("vtext2").innerHTML = value2 + " Pontos.";
 
     if (vArrests === "5" && vInput >= 1  || vArrests === "10" && vInput >= 1 || vArrests === "12" && vInput >= 1 ){
 
@@ -25,8 +28,16 @@ function arrests() {
         document.getElementById("vtext2").innerHTML = "--> Digite a quantidade em gramas " 
       
      }
-    
-    
- 
+
      
+
+     
+     document.getElementById("vtotal").innerHTML = parseInt(situations()) + value2 +  " Pontos"
+
 }
+
+
+
+
+
+
